5d6244250ae27ef572d48635aef68570320fa3e6
gingeralesy <gingeralesy@gmail.com>
2023-12-01 10:03:00 +0200

    File line iterator

--- util.lisp
+++ util.lisp
@@ -20,6 +20,24 @@
         (or (and match groups (aref groups 0)) ""))
       line))
 
+(defmacro do-file ((line-var filename &optional return-value) &body body)
+  (let ((file-var (gensym "FILE"))
+        (filename-var (gensym "FILENAME"))
+        (stream-var (gensym "STREAM"))
+        (raw-line-var (gensym "LINE")))
+    `(let* ((,filename-var ,filename)
+            (,file-var (local-file ,filename-var :error T)))
+       (with-open-file (,stream-var ,file-var :direction :input
+                                              :element-type 'character
+                                              :if-does-not-exist :error
+                                              :external-format :utf-8)
+         (loop for ,raw-line-var of-type input-line = (read-line ,stream-var NIL :eof)
+               until (eql ,raw-line-var :eof)
+               do (let ((,line-var (clean-line ,raw-line-var)))
+                    (declare (type input-line ,line-var))
+                    ,@body)
+               finally (return ,return-value))))))
+
 (declaim (inline u32 u64))
 (defun u32 (value) (logand #xffffffff value))
 (defun u64 (value) (logand #xffffffffffffffff value))
