51d09ad389b22a1ee5daa22954a0b89851870889
gingeralesy <gingeralesy@gmail.com>
2023-12-03 13:31:16 +0200

    Day 3 rewrite

--- day3.lisp
+++ day3.lisp
@@ -3,95 +3,81 @@
 ;; https://adventofcode.com/2023/day/3
 
 (defun d3-data ()
-  (declare (optimize (speed 3)))
-  (let ((schematic ())
-        (tail ())
-        (width 0)
-        (height 0))
-    (declare (type (unsigned-byte 32) width height))
-    (do-file (line "day3.txt")
-      (unless (< 0 width)
-        (setf width (length line)))
-      (loop with y = height
-            for x from 0 below width
-            collect (char line x) into row
-            finally (cond
-                      (schematic
-                       (setf (cdr tail) (list row))
-                       (setf tail (cdr tail)))
-                      (T
-                       (setf schematic (list row))
-                       (setf tail schematic))))
-      (incf height))
-    (values (make-array (list height width) :element-type 'character :initial-contents schematic)
-            width height)))
+  (let ((num-re (cl-ppcre:create-scanner "\\b\\d+\\b"))
+        (sym-re (cl-ppcre:create-scanner "[^\\d\\.]"))
+        (schematic ())
+        (tail ()))
+    (do-file (line "day3.txt" schematic)
+      (let ((row (list line)))
+        (loop for prev = 0 then end
+              for (start end) = (multiple-value-list (cl-ppcre:scan num-re line :start prev))
+              while (and start end)
+              collect (cons start end) into nums
+              finally (push nums row))
+        (loop for prev = 0 then end
+              for (start end) = (multiple-value-list (cl-ppcre:scan sym-re line :start prev))
+              while (and start end)
+              collect start into syms
+              finally (push syms row))
+        (cond
+          (schematic
+           (setf (cdr tail) (list row))
+           (setf tail (cdr tail)))
+          (T
+           (setf schematic (list row))
+           (setf tail schematic)))))))
 
-(declaim (inline d3-node))
-(defun d3-node (x y schematic width height)
-  (when (and (<= 0 x) (< x width) (<= 0 y) (< y height))
-    (aref schematic y x)))
+(defmacro do-d3-schematic (((prev-syms-var prev-nums-var prev-line-var)
+                            (curr-syms-var curr-nums-var curr-line-var)
+                            (next-syms-var next-nums-var next-line-var)
+                            schematic &optional return-value)
+                           &body body)
+  (let ((prev-var (gensym "PREV"))
+        (curr-var (gensym "CURR")))
+    `(loop for ,prev-var = NIL then ,curr-var
+           for ,curr-var on ,schematic
+           for (,prev-syms-var ,prev-nums-var ,prev-line-var) = (car ,prev-var)
+           for (,curr-syms-var ,curr-nums-var ,curr-line-var) = (car ,curr-var)
+           for (,next-syms-var ,next-nums-var ,next-line-var) = (cadr ,curr-var)
+           do (progn ,@body)
+           finally (return ,return-value))))
 
 (defun d3p1 ()
-  (multiple-value-bind (schematic width height) (d3-data)
-    (labels ((node (x y) (d3-node x y schematic width height))
-             (symbol-node-p (x y)
-               (let ((char (node x y)))
-                 (and char (not (or (digit-char-p char) (char= char #\.)))))))
-      (let ((nums ()))
-        (dotimes (y height (apply #'+ nums))
-          (loop for x = 0 then (+ x num-width)
-                while (< x width)
-                for char = (or (node x y) (error "Missing character: ~d,~d" x y))
-                for num-width = (if (digit-char-p char)
-                                    (loop for w from 1
-                                          for next = (node (+ x w) y)
-                                          while (and next (digit-char-p next))
-                                          finally (return w))
-                                    1)
-                when (and (digit-char-p char)
-                          (loop with has-symbol-p = NIL
-                                until has-symbol-p
-                                for dy from -1 upto 1
-                                do (loop for dx from -1 upto num-width
-                                         until has-symbol-p
-                                         do (setf has-symbol-p (symbol-node-p (+ x dx) (+ y dy))))
-                                finally (return has-symbol-p)))
-                collect (loop for i from 0 below num-width
-                              collecting (node (+ x i) y) into num
-                              finally (return (parse-integer (format NIL "~{~c~}" num))))
-                into row-nums
-                finally (setf nums (nconc nums row-nums))))))))
+  (let ((schematic (d3-data))
+        (sum 0))
+    (flet ((has-sym-p (start end syms)
+             (dolist (sym syms)
+               (when (and (<= start sym) (< sym end))
+                 (return T)))))
+      (do-d3-schematic ((prev-syms prev-nums prev-line)
+                        (curr-syms curr-nums curr-line)
+                        (next-syms next-nums next-line)
+                        schematic sum)
+        (loop for (start-num . end-num) in curr-nums
+              when (or (has-sym-p (1- start-num) (1+ end-num) prev-syms)
+                       (has-sym-p (1- start-num) (1+ end-num) curr-syms)
+                       (has-sym-p (1- start-num) (1+ end-num) next-syms))
+              do (incf sum (parse-integer (subseq curr-line start-num end-num))))))))
 
 ;; Answer: 517021
 
 (defun d3p2 ()
-  (multiple-value-bind (schematic width height) (d3-data)
-    (labels ((node (x y) (d3-node x y schematic width height))
-             (num-at (x y)
-               (when (digit-char-p (node x y))
-                 (let ((num-start (loop for x0 = (1- x) then (1- x0)
-                                        for prev = (node x0 y)
-                                        while (and prev (digit-char-p prev))
-                                        finally (return (1+ x0))))
-                       (num-end (loop for x0 from (1+ x)
-                                      for next = (node x0 y)
-                                      while (and next (digit-char-p next))
-                                      finally (return x0))))
-                   (loop for x0 from num-start below num-end
-                         collect (node x0 y) into num
-                         finally (return (values (parse-integer (format NIL "~{~c~}" num))
-                                                 num-end)))))))
-      (let ((gears ()))
-        (dotimes (y height (apply #'+ gears))
-          (dotimes (x width)
-            (when (char= #\* (or (node x y) (error "Missing character: ~d,~d" x y)))
-              (loop with gear-nums = ()
-                    for y0 from (1- y) upto (1+ y)
-                    do (loop for x0 = (1- x) then (if num (1+ num-end) (1+ x0))
-                             until (< (1+ x) x0)
-                             for (num num-end) = (multiple-value-list (num-at x0 y0))
-                             when num do (push num gear-nums))
-                    finally (when (< 1 (length gear-nums))
-                              (push (apply #'* gear-nums) gears))))))))))
+  (let ((schematic (d3-data))
+        (sum 0))
+    (flet ((nums-for (x nums line)
+             (loop for (start . end) in nums
+                   when (and (<= start (1+ x)) (<= x end))
+                   collect (parse-integer (subseq line start end)))))
+      (do-d3-schematic ((prev-syms prev-nums prev-line)
+                        (curr-syms curr-nums curr-line)
+                        (next-syms next-nums next-line)
+                        schematic sum)
+        (loop for sym in curr-syms
+              when (char= #\* (char curr-line sym))
+              do (let ((nums (nconc (nums-for sym prev-nums prev-line)
+                                    (nums-for sym curr-nums curr-line)
+                                    (nums-for sym next-nums next-line))))
+                   (when (< 1 (length nums))
+                     (incf sum (apply #'* nums)))))))))
 
 ;; Answer: 81296995
